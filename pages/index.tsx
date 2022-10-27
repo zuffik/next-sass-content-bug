ad17dd4c33720bef57cd98064b03e789df2ac6a4
Kristian Zuffa <kristian.zuffa@gmail.com>
2022-10-27 10:29:14 +0200

    bug init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
+import classes from './App.module.scss';
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return <div className={classes.item}>test</div>
 }
